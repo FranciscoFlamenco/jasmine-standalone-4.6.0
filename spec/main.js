4ecf5a74fefeb033608c34a2dcb7dd8f5add894d
FranciscoFlamenco <A01732742@tec.mx>
2024-05-13 13:40:33 -0400

    Version 3: Prueba 3

--- spec/main.js
+++ spec/main.js
@@ -1,16 +1,23 @@
-function insertDashes(str){
-	// write code here
+class User {
+  firstName;
+  lastName;
+  middleName;
+  constructor(data = {}) {
+    this.firstName = data.firstName;
+    this.lastName = data.lastName || '';
+    this.middleName = data.middleName;
+  }
 }
-
-describe('Test Example', () => {
-	it('insert dashes in between chars', () => {
-		const value = "aba caba"
-
-		const result = insertDashes(value)
-
-		console.log("result: ", result)
-
-		expect(result).toBe(undefined)
-
-	});
+/**
+ * Test Suite
+ */
+describe(`${User.name} Class`, () => {
+  it("first name defaults to empty", () => {
+    // arrange
+    const data = { firstName: null };
+    // act
+    const model = new User(data);
+    // assert
+    expect(model.firstName).toBe('');
+  });
 });
